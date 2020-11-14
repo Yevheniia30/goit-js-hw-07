488665dfb0efa20cdf84f80c60f2735ad06f0c9b
Evgeniya3010 <susskaya86@gmail.com>
2020-11-14 23:46:59 +0200

    решение 3-й задачи

--- js/task-03.js
+++ js/task-03.js
@@ -16,8 +16,42 @@ const images = [
   },
 ];
 
-function getImagesMarkup(arr) {
-  const gallery = `
-  <ul>
-  <li>`
-}
+// способ 1 - метод insertAdjacentHTML и шаблонная строка
+
+const galleryList = document.querySelector('#gallery');
+// console.log(galleryList);
+galleryList.insertAdjacentHTML('beforeend', images.map(image => `<li><img src = "${image.url}" alt = "${image.alt}"></li>`).join(""))
+console.log(galleryList)
+
+// присваиваем класс списку
+galleryList.classList.add('js-images-list');
+
+// присваиваем класс всем элементам списка
+const imagesItems = document.querySelectorAll('li');
+// console.log(imagesItems);
+imagesItems.forEach(imagesItem => {
+  imagesItem.classList.add('js-images-item')
+})
+
+
+
+// способ 2 - методы createElement, append
+
+// const imagesList = images.forEach(image => {
+//   const imagesItem = document.createElement('li');
+//   const imagesItemEl = document.createElement('img');
+
+//   imagesItemEl.setAttribute('src', image.url);
+//   imagesItemEl.setAttribute('alt', image.alt);
+
+//   imagesItem.append(imagesItemEl);
+//   galleryList.append(imagesItem);
+
+//   imagesItem.classList.add('js-images-item');
+//   galleryList.classList.add('js-images-list');
+// })
+
+
+
+
+
