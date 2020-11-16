f22da6043a1fd987236b56086ab4ecfe5cd4396a
Evgeniya3010 <susskaya86@gmail.com>
2020-11-16 03:17:51 +0200

    решение 5-й задачи с использованием тернарного оператора

--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,16 @@
 const inputRef = document.querySelector('#name-input')
-console.log(inputRef)
 const outputRef = document.querySelector('#name-output')
-console.log(outputRef);
+
 // const titleRef = document.querySelector('h1')
 // console.log(titleRef);
 
-inputRef.addEventListener('input', event=> {
-   outputRef.textContent = event.target.value;
-}) 
+// способ 1
+// inputRef.addEventListener('input', event=> {
+//    outputRef.textContent = event.target.value;
+// }) 
 
+const inputEvent = event => {
+    inputRef.value ? outputRef.textContent = event.target.value : outputRef.textContent='незнакомец'
+}
 
-
+inputRef.addEventListener('input', inputEvent);
