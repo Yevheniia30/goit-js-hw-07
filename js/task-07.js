a98e1bfb93b188b1669ed38f5cee955f9fd9935a
Evgeniya3010 <susskaya86@gmail.com>
2020-11-18 06:09:58 +0200

    решение 7-й задачи

--- js/task-07.js
+++ js/task-07.js
@@ -1,4 +1,15 @@
 const inputRef = document.querySelector("#font-size-control");
 console.log(inputRef);
 
-const 
+const textRef = document.querySelector("#text");
+console.log(textRef);
+
+// inputRef.addEventListener("input", (event) => {
+//   textRef.style.fontSize = `${inputRef.value}px`;
+// });
+
+const inputHandler = (event) => {
+  textRef.style.fontSize = `${inputRef.value}px`;
+};
+
+inputRef.addEventListener("input", inputHandler);
