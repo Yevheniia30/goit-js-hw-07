56597e6f289069afd3b775f0f6602c0ae0067602
Evgeniya3010 <susskaya86@gmail.com>
2020-11-19 19:02:50 +0200

    форматирование кавычек

--- js/task-03.js
+++ js/task-03.js
@@ -1,57 +1,57 @@
 const images = [
   {
     url:
-      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
     url:
-      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
     url:
-      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 
 // способ 1 - метод insertAdjacentHTML и шаблонная строка
 
-const galleryList = document.querySelector('#gallery');
+const galleryList = document.querySelector("#gallery");
 // console.log(galleryList);
-galleryList.insertAdjacentHTML('beforeend', images.map(image => `<li><img src = "${image.url}" alt = "${image.alt}"></li>`).join(""))
-console.log(galleryList)
+galleryList.insertAdjacentHTML(
+  "beforeend",
+  images
+    .map((image) => `<li><img src = "${image.url}" alt = "${image.alt}"></li>`)
+    .join("")
+);
+console.log(galleryList);
 
 // присваиваем класс списку
-galleryList.classList.add('js-images-list');
+galleryList.classList.add("js-images-list");
 
 // присваиваем класс всем элементам списка
-const imagesItems = document.querySelectorAll('li');
-// console.log(imagesItems);
-imagesItems.forEach(imagesItem => {
-  imagesItem.classList.add('js-images-item')
-})
-
-
+const imagesItems = document.querySelectorAll("li");
+console.log(imagesItems);
+imagesItems.forEach((imagesItem) => {
+  imagesItem.classList.add("js-images-item");
+});
 
 // способ 2 - методы createElement, append
 
-// const imagesList = images.forEach(image => {
-//   const imagesItem = document.createElement('li');
-//   const imagesItemEl = document.createElement('img');
+// const imagesList = images.forEach((image) => {
+//   const imagesItem = document.createElement("li");
+//   const imagesItemEl = document.createElement("img");
 
-//   imagesItemEl.setAttribute('src', image.url);
-//   imagesItemEl.setAttribute('alt', image.alt);
+//   imagesItemEl.setAttribute("src", image.url);
+//   imagesItemEl.setAttribute("alt", image.alt);
 
 //   imagesItem.append(imagesItemEl);
 //   galleryList.append(imagesItem);
 
-//   imagesItem.classList.add('js-images-item');
-//   galleryList.classList.add('js-images-list');
-// })
+//   imagesItem.classList.add("js-images-item");
+//   galleryList.classList.add("js-images-list");
 
-
-
-
-
+//   console.log(galleryList);
+// });
